0db00f8d26c12c341433bf423758e2bd931fead5
Clark Fischer <spacestationfour@gmail.com>
2021-02-09 22:19:23 -0500

    Still trying to fix that error

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,7 @@ var mongoSanitize = require('mongo-express-sanitize');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
-  res.render('helloworld', { title: 'Hello, World!' });
+  res.render('userlist', { title: 'Express' });
 });
 /* GET Userlist page. */
 router.get('/userlist', function(req, res) {
@@ -21,52 +17,6 @@ router.get('/userlist', function(req, res) {
       });
   });
 });
-router.get('/mainPage', function(req, res) {
-  var db = req.db;
-  var collection = db.get('usercollection');
-  collection.find({},{},function(e,docs){
-      res.render('mainPage', {
-          "mainPage" : docs
-      });
-  });
-});
-router.get('/newuser', function(req, res) {
-  res.render('newuser', { title: 'Add New User' });
-});
-
-/* POST to Add User Service */
-// router.post('/addinput', function(req, res) {
-
-//   // Set our internal DB variable
-//   var db = req.db;
-
-//   // Get our form values. These rely on the "name" attributes
-//   var usersName = req.body.usersName;
-//   var projectName = req.body.projectName;
-//   var contactPref = req.body.contactPref;
-//   var contactName = req.body.contactName;
-
-//   // Set our collection
-//   var collection = db.get('usercollection');
-
-//   // Submit to the DB
-//   collection.insert({
-//       "usersName" : usersName,
-//       "projectName" : projectName,
-//       "contactPref": contactPref,
-//       "contactName" : contactName
-//   }, function (err, doc) {
-//       if (err) {
-//           // If it failed, return error
-//           res.send("There was a problem adding the information to the database.");
-//       }
-//       else {
-//           // And forward to success page
-//           res.redirect("userlist");
-//       }
-//   });
-
-// });
 router.post('/adduser', function(req, res) {
 
   // Set our internal DB variable
